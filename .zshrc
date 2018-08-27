18677c957066f5463ef103b6e40c793f472ed6f9
michaelchen110 <puching110@gmail.com>
2018-08-27 09:15:36 -0700

    leetcode.py

--- .zshrc
+++ .zshrc
@@ -11,6 +11,9 @@ export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export LANG=en_US.UTF-8 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
 # Set Vim as default editor
 export EDITOR=vim
 
--- lib/leetcode.py
+++ lib/leetcode.py
@@ -1,96 +1,106 @@
+""" 
+@ Redo again
+
+https://www.lintcode.com/problem/window-sum/description
+https://www.lintcode.com/problem/triangle-count/description
+
+@ Review Later
+https://www.jiuzhang.com/tutorial/segment-tree/237
+https://www.jiuzhang.com/solution/strstr-ii/#tag-highlight-lang-python
+
+
+"""
+
+
+
 # TODO Pow(x, n)
 # -1 // 2, special numerical operation handling
 # see how others handle math problem
 # deal with n < 2, e.g. n == -1 , n == 0
 
-### PYTHON  
+# NOTE 
+# python + operator used when O(1)
 
-# Comparison 
-# == -> value equal
-# is -> reference equal
-if x: pass
-if x != None: pass
-if x is not None: pass
 
-# False values
-0, None, [], {}, ""
+"""
+                                Corner Cases                
+"""
+# BFS
+while queue: continue
 
+# DFS/BT/BST
+if root is None: return None
+if tree_node is not None: continue
+
+# Linked List
+if not head or not head.next: return None
+
+                          
+"""                             TYPE CHECKING 
+value equal: ==, != 
+reference equal: is, is not
+False: 0, "", (), {}, [] or None
+True: Others
+Check false:
+    if x is None: pass
+    if x == 0: pass
+    if x == []: pass
+    if x == {}: pass
+    if not input: pass
+"""
 # Logical Operator
 and, or, not
 
-# zip
-zipped = list(zip([1,2], [3,4])) # [(1,3), (2,4)]
-unzipped = list(zip(*zipped)) #[(1,2), (3,4)]
-
-# remove substring
-str.strip("pattern")
-# split
-str.split(',')
-# divide to integer
-5 // 2 == 2
-5 / 2 == 2.5
-
+# Numeric type
+# Integers have unlimited precision.
+import sys
 sys.maxsize
+-sys.maxsize-1
 
-# Catch "", [] or None
-input = "" 
-if not input: return input
-# Hash has key
-if dict.get(key) is not None: 
+abs(x)
+int(x)
+float(x)
+5 / 2 == 2.5
+# The result is always rounded towards minus infinity: 
+5//2 == 2
+1//2 ==  0
+(-1)//2 == -1 
+1//(-2) == -1 
+(-1)//(-2) == 0
 
-# Literals
-Tuples: (1, 2, 3)
-Lists: [1, 2, 3]
-Dicts: {1: 'one', 2: 'two'}
-Sets: {1, 2, 3}
+""" 
+                            SEQUENCE OPERATION 
+"""
+min(sequence)
+max(sequence)
+len(sequence)
 
-# Functional Programming
-add = lambda x, y: x + y
-add(1, 2)
-# Map
-list(map(add, [(1,2), (3,4)]))
-# Filter
-list(filter(lambda x: x < 0, range(-10, 10)))
-# Reduce
-from functools import reduce
-product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
+# initialize sequence
+[False] * 5 # 1D init
+[[0] * 3 for _ in range(10)] # 2D init
 
-def lexical_scoping(x):
-    def adder(y):
-        return x + y
-    return adder
-lexical_scoping(3)(4)
-add3 = lexical_scoping(3)
-add3(4) == 7
-
-# confusing 
-i = 4
-def foo(x):
-    def bar():
-        print(i, end=' ')
-    for i in x:  # i *is* local to Foo, so this is what Bar sees
-        print(i, end=' ')
-    bar()
-foo([1,2,3]) # 1 2 3 3
-
-# Unpack argument list
-def unpack(self, *args, **kwargs)
-args = [1,5]
-kwargs = {"a": 10, "b": 20}
+# concat sequence
+list1 + list2
 
 # String
 s.upper() 
 s.lower()
 s.replace("old", "new")
-s.find("substring")
+s.find("substring") # return index
+"substring" in s # return boolean
 s.isalpha()
 s.isdigit()
 s.split(",")
 ",".join(['first', 'second'])
 ord('a') == 97
+# remove substring
+str.strip("pattern")
+# split
+str.split(',')
 
 # List
 l.append(x) # equals to l[len(l):] = [x]
+l.insert(i, x)
 l.sort() # in-place
 l.sort(reverse=True)
 l.sort(key=lambda x: x[1])
@@ -109,31 +119,35 @@ nums.reverse() # in-place
 nums[::-1] # new object 
 nums[:1:-2] == [5, 3] # first spot is infered as the last number
 
-# initialize
-[False] * 5 # 1D init
-[[0] * 3 for _ in range(10)] # 2D init
 
-# concat
-list1 + list2
-
+""" 
+                            DATA STRUCTURE 
+"""
 # Stack [ ...  top]
 stack = [1,2] # 
 stack.append(3) # [1,2,3]
 stack.pop()
 
 # Queue [ enqueue ... dequeue]
-queue = [1,2]
-queue.insert(0, 3) # [3, 1, 2]
-queue.pop() # [3, 1]
+from collections import deque
+queue = deque([])
+queue.append(3)
+queue.popleft()
 
 # Hash
 hash = {}
+hash = {x: 0 for x in range(10)}
+len(hash)
 hash[key] = 10
 hash.get(key) # 10
 hash.get(key, 20) # getOrDefault
 del hash[key]
-len(hash)
 for k, v in hash.iteritems():
+# Check hash contains key  
+if dict_.get(key) is not None: pass
+# Sequence (e.g. list, dict, tuple, str) Contains
+if key in dict_: pass
+if e not in list_: pass
 
 # RBTree
 from collections import OrderedDict
@@ -146,34 +160,16 @@ s.discard(10)
 x in s
 len(s)
 
-# BFS
-if not root: 
-    return 0
-queue = [root]
-
-while len(queue) > 0:
-    num = len(queue)
-    for i in range(num - 1, -1, -1):
-        node = queue.pop()
-        if node.left:
-            queue.insert(0, node.left)
-        if node.right:
-            queue.insert(0, node.right)
-
 # LINKED LIST
 
-# BINARY TREE
-
-# BINARY SEARCH
-
+""" 
+                                BINARY SEARCH
+"""
+# OOOOO[O] [X]XXXXXX
 # [1,1] find 1 inf loop if start < end
 def bsearch(self, nums, target):
     if not nums: 
         return -1
-<<<<<<< HEAD
-    start, end = 0, len(nums) - 1
-=======
->>>>>>> a003b50ff733b3941dfb659552840fc5efa4bbc1
 
     start, end = 0, len(nums) - 1
     while start + 1 < end;
@@ -182,11 +178,8 @@ def bsearch(self, nums, target):
             start = mid
         elif nums[mid] > target:
             end = mid
-<<<<<<< HEAD
         else:
             return mid
-=======
->>>>>>> a003b50ff733b3941dfb659552840fc5efa4bbc1
 
     if nums[start] == target:
         return start
@@ -194,57 +187,217 @@ def bsearch(self, nums, target):
         return end
     return -1
 
-# QUICK SORT
 
-def qsort(self, nums, low, high):
-    # import random
-    # idx = random.choice(range(len(nums)))
-    # nums[idx], nums[high] = nums[high], nums[idx]
-    pivot = nums[(low+high)//2]
+"""
+                                TWO POINTERS
+main idea:
+    swap with left or right when idx is not in the correct part
+"""
 
-    mid = low
-    for i in range(low, high):
-        if nums[i] < pivot:
-            nums[mid], nums[i] = nums[i], nums[mid]
-            mid += 1
+"""
+TWO POINTERS -> <- 
+"""
 
-    nums[mid], nums[high] = nums[high], nums[mid]
-    # qsort
-    self.qsort(nums, low, mid - 1)
-    self.qsort(nums, mid + 1, high)
-
-    # qselect kth idx
-    if k < mid:
-        self.qselect(nums, low, mid - 1, k)
-    elif k > mid:
-        self.qselect(nums, mid + 1, high, k)
+### TWO SUM related problems
+left, right = 0, len(nums) - 1
+while left <= right:
+    if nums[left] < pivot:
+        left += 1
+    elif nums[right] >= pivot: # quick sort use > pivot
+        right -= 1
     else:
-        return pivot
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
 
+# unstable
+# Two way partition : ... right | left  ... 
+# can swap left pivot and right pivot to balance the split
+left, right = 0, len(nums) - 1
+while left <= right:
+    while left <= right and nums[left] < pivot:
+        left += 1
+    while left <= right and nums[right] > pivot:
+        right -= 1
+    if left <= right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
 
-# Three way partitioning: small | low _ pivot _ high | large
-i = low
-while i <= high:
-    while i <= high and nums[i] > nums[high]:
-        nums[i], nums[high] = nums[high], nums[i]
-        high -= 1
-    # two way partition
-    if nums[i] < nums[low]:
-        nums[i], nums[low] = nums[low], nums[i]
-        low += 1
+"""
+TWO POINTERS -> -> 
+"""
+# stable
+# Two way partition : ... | left  ... 
+left, right = 0, 0
+while right < len(nums):
+    if nums[right] < pivot:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+    right += 1
+
+# left = 0
+# for i in range(len(nums)):
+    # # nums[i] is left part
+    # if nums[i] < pivot:
+        # nums[left], nums[i] = nums[i], nums[left] 
+        # left += 1
+
+# Three way partition: ... | left ... right | ...
+left, i, right = 0, 0, len(nums) - 1
+while i <= right:
+    # nums[i] is right part
+    while i <= right and nums[i] > pivot:
+        nums[i], nums[right] = nums[right], nums[i]
+        right -= 1
+    # nums[i] is left part
+    if nums[i] < pivot:
+        nums[i], nums[left] = nums[left], nums[i]
+        left += 1
     i += 1
 
-# Palindrome
+# left, right = 0, len(nums) - 1
+# for i in range(len(nums)):
+    # if i > right:
+        # break
+    # while i <= right and nums[i] > pivot:
+        # nums[i], nums[right] = nums[right], nums[i]
+        # right -= 1
+    # if nums[i] < pivot:
+        # nums[i], nums[left] = nums[left], nums[i]
+        # left += 1
 
+# while i <= right:
+    # if A[i] < pivot:
+        # A[left], A[i] = A[i], A[left]
+        # left += 1
+        # i += 1
+    # elif A[i] == pivot:
+        # i += 1
+    # elif A[i] > pivot:
+        # A[right], A[i] = A[i], A[right]
+        # right -= 1
+
+# QUICK SORT
+def qsort(self, nums, start, end):
+    if start >= end:
+        return
+
+    pivot = nums[(start + end) // 2]
+
+    left, right = start, end
+    while left <= right: 
+        while left <= right and nums[left] < pivot:
+            left += 1
+        while left <= right and nums[right] > pivot:
+            right -= 1
+        if left <= right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+    left, right = right, left
+    # qsort  
+    self.qsort(nums, start, left)
+    self.qsort(nums, right, end)
+ 
+    # qselect kth idx
+    if k <= left:
+        self.qselect(nums, start, left, k)
+    elif k >= right:
+        self.qselect(nums, right, end, k)
+    else: 
+        return pivot
+
+### Other Sorting
+def bubble(nums):
+    # nums[end] will be the largest in range(0, end+1)
+    # From end - 1 to 1
+    for end in range(len(nums)-1, 0, -1):
+        # From 0 to end-1
+        for i in range(end):
+            if nums[i] > nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+
+def insertion(nums):
+    for end in range(1, len(nums)):
+        i = end
+        # optimized, less write operation 
+        tmp = nums[i]
+        while i > 0 and nums[i-1] > nums[i]:
+            nums[i] = nums[i-1]
+            i -= 1
+        nums[i] = tmp
+
+        # swap
+        while i > 0 and nums[i-1] > nums[i]:
+            nums[i-1], nums[i] = nums[i], nums[i-1]
+            i -= 1
+
+"""
+TWO POINTERS <- ->  
+"""
+# Palindrome
 left, right = middle, middle # odd
 left, right = middle, middle + 1 # even
 while low >= 0 and high < len(s) and s[low] == s[high]:
     low -= 1
     high += 1
-<<<<<<< HEAD
-len = high - low - 1
-
-# Bsearch
-=======
 return low >= high # True if palindrom
->>>>>>> a003b50ff733b3941dfb659552840fc5efa4bbc1
+
+
+""" 
+                                    BFS 
+"""
+from collections import deque
+if not root: 
+    return 0
+queue = deque([root])
+
+while len(queue) > 0:
+    num = len(queue)
+    for i in range(num - 1, -1, -1):
+        node = queue.popleft()
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+
+"""
+                                    DFS
+"""
+
+# BST, INORDER
+class BSTIterator:
+    """
+    @param: root: The root of binary tree.
+    """
+    def __init__(self, root):
+        self.stack = []
+        while root != None:
+            self.stack.append(root)
+            root = root.left
+
+    """
+    @return: True if there has next node, or false
+    """
+    def hasNext(self):
+        return len(self.stack) > 0
+
+    """
+    @return: return next node
+    """
+    def next(self):
+        node = self.stack[-1]
+        if node.right is not None:
+            n = node.right
+            while n != None:
+                self.stack.append(n)
+                n = n.left
+        else:
+            n = self.stack.pop()
+            while self.stack and self.stack[-1].right == n:
+                n = self.stack.pop()
+        
+        return node
