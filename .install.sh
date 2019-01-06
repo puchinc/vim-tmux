1647dd8f2421fabf5e98b43577eda5c42b11e0e0
michaelchen110 <puching110@gmail.com>
2019-01-05 23:59:14 -0500

    revised

--- .install.sh
+++ .install.sh
@@ -13,6 +13,7 @@ if [ "$(uname)" == "Darwin" ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update
     brew install vim && brew install macvim
+    brew install npm
     ln -s /usr/local/bin/mvim vim
     brew install zsh
     brew install tmux
@@ -43,7 +44,9 @@ jupyter nbextension enable vim_binding/vim_binding
 # ZSH
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo echo "$(which zsh)" >> /etc/shells
 chsh -s $(which zsh)
+
 source ~/.zshrc
 npm install --global pure-prompt
 
@@ -52,3 +55,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe && ./install.py
 cd -
+
+# Autojump
+git clone git://github.com/wting/autojump.git
+cd autojump
+./install.py
+cd - && rm -rf autojump
